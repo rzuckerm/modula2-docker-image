91564c9b1dd6e84fb3e8ff2e597e8c0988bfc679
Ron Zuckerman <ron.zuckerman@adtran.com>
2023-08-18 20:31:08 -0500

    Remove some more extraneous files

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ DOCKER_RUN="docker run --rm -i -v $(pwd):/local -w /local ${DOCKER_IMAGE}"
 
 CMD="gm2 -fiso hello_world.mod -o hello_world && ./hello_world"
 RESULT="$(${DOCKER_RUN} sh -c "${CMD}")"
-rm -f hello_world
+rm -f hello_world hello_world*.o
 echo "${RESULT}"
 if [ "${RESULT}" = "Hello, world!" ]
 then
